403eee2294801150bcf10ca90df6f2e45693015b
Eliott Mischler <emischler2001@gmail.com>
2022-06-30 10:41:18 +0200

    "mongodb initialized maven project, docker-compose.yml running mongo image"

---
+++ mediscreen-notes/src/test/java/com/demo/mediscreennotes/MediscreenNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.mediscreennotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediscreenNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
